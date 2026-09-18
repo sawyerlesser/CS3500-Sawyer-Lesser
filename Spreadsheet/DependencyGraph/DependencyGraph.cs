0fe834d59ec9e769852451bf7b362b11d6189231
sawyerlessser <sawyerlesser@gmail.com>
2026-09-18 17:02:44 -0600

    PS3 Complete

---
+++ Spreadsheet/DependencyGraph/DependencyGraph.cs
@@ -0,0 +1,245 @@
+﻿// Skeleton implementation written by Joe Zachary for CS 3500, September 2013
+// Version 1.1 - Joe Zachary
+//   (Fixed error in comment for RemoveDependency)
+// Version 1.2 - Daniel Kopta Fall 2018
+//   (Clarified meaning of dependent and dependee)
+//   (Clarified names in solution/project structure)
+// Version 1.3 - H. James de St. Germain Fall 2024
+
+namespace DependencyGraph;
+
+/// <summary>
+///   <para>
+///     (s1,t1) is an ordered pair of strings, meaning t1 depends on s1.
+///     (in other words: s1 must be evaluated before t1.)
+///   </para>
+///   <para>
+///     A DependencyGraph can be modeled as a set of ordered pairs of strings.
+///     Two ordered pairs (s1,t1) and (s2,t2) are considered equal if and only
+///     if s1 equals s2 and t1 equals t2.
+///   </para>
+///   <remarks>
+///     Recall that sets never contain duplicates.
+///     If an attempt is made to add an element to a set, and the element is already
+///     in the set, the set remains unchanged.
+///   </remarks>
+///   <para>
+///     Given a DependencyGraph DG:
+///   </para>
+///   <list type="number">
+///     <item>
+///       If s is a string, the set of all strings t such that (s,t) is in DG is called dependents(s).
+///       (The set of things that depend on s.)
+///     </item>
+///     <item>
+///       If s is a string, the set of all strings t such that (t,s) is in DG is called dependees(s).
+///       (The set of things that s depends on.)
+///     </item>
+///   </list>
+///   <para>
+///      For example, suppose DG = {("a", "b"), ("a", "c"), ("b", "d"), ("d", "d")}.
+///   </para>
+///   <code>
+///     dependents("a") = {"b", "c"}
+///     dependents("b") = {"d"}
+///     dependents("c") = {}
+///     dependents("d") = {"d"}
+///     dependees("a")  = {}
+///     dependees("b")  = {"a"}
+///     dependees("c")  = {"a"}
+///     dependees("d")  = {"b", "d"}
+///   </code>
+/// </summary>
+public class DependencyGraph
+{
+    private readonly Dictionary<string, HashSet<string>> _dependents;
+    private readonly Dictionary<string, HashSet<string>> _dependees;
+    private int _size;
+    
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="DependencyGraph"/> class.
+    ///   The initial DependencyGraph is empty.
+    /// </summary>
+    public DependencyGraph()
+    {
+        _dependents = new Dictionary<string, HashSet<string>>();
+        _dependees = new Dictionary<string, HashSet<string>>();
+        _size = 0;
+    }
+
+    /// <summary>
+    /// The number of ordered pairs in the DependencyGraph.
+    /// </summary>
+    public int Size => _size;
+
+    /// <summary>
+    ///   Reports whether the given node has dependents (i.e., other nodes depend on it).
+    /// </summary>
+    /// <param name="nodeName"> The name of the node.</param>
+    /// <returns> true if the node has dependents. </returns>
+    public bool HasDependents(string nodeName)
+    {
+        if (_dependents.TryGetValue(nodeName, out var dependent))
+        {
+            if (dependent.Count > 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /// <summary>
+    ///   Reports whether the given node has dependees (i.e., depends on one or more other nodes).
+    /// </summary>
+    /// <returns> true if the node has dependees.</returns>
+    /// <param name="nodeName">The name of the node.</param>
+    public bool HasDependees(string nodeName)
+    {
+        if (_dependees.TryGetValue(nodeName, out var dependee))
+        {
+            if (dependee.Count > 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    ///   <para>
+    ///     Returns the dependents of the node with the given name.
+    ///   </para>
+    /// </summary>
+    /// <param name="nodeName"> The node we are looking at.</param>
+    /// <returns> The dependents of nodeName. </returns>
+    public IEnumerable<string> GetDependents(string nodeName)
+    {
+        if (_dependents.TryGetValue(nodeName, out var dependents))
+        {
+            return dependents;
+        }
+        
+        return new HashSet<string>();
+    }
+
+    /// <summary>
+    ///   <para>
+    ///     Returns the dependees of the node with the given name.
+    ///   </para>
+    /// </summary>
+    /// <param name="nodeName"> The node we are looking at.</param>
+    /// <returns> The dependees of nodeName. </returns>
+    public IEnumerable<string> GetDependees(string nodeName)
+    {
+        if (_dependees.TryGetValue(nodeName, out var dependees))
+        {
+            return dependees;
+        }
+        
+        return new HashSet<string>();
+    }
+
+    /// <summary>
+    /// <para>Adds the ordered pair (dependee, dependent), if it doesn't exist.</para>
+    ///
+    /// <para>
+    ///   This can be thought of as: dependee must be evaluated before dependent
+    /// </para>
+    /// </summary>
+    /// <param name="dependee"> the name of the node that must be evaluated first</param>
+    /// <param name="dependent"> the name of the node that cannot be evaluated until after dependee</param>
+    public void AddDependency(string dependee, string dependent)
+    {
+        if (this._dependees.ContainsKey(dependent) &&
+            this._dependees[dependent].Contains(dependee))
+        {
+            return;
+        }
+        if (this._dependees.TryGetValue(dependent, out var dependee1))
+        {
+            dependee1.Add(dependee);
+        }
+        else
+        {
+            this._dependees.Add(dependent, new HashSet<string>());
+            this._dependees[dependent].Add(dependee);
+        }
+
+        if (this._dependents.TryGetValue(dependee, out var dependent1))
+        {
+            dependent1.Add(dependent);
+        }
+        else
+        {
+            this._dependents.Add(dependee, new HashSet<string>());
+            this._dependents[dependee].Add(dependent);
+        }
+
+        _size++;
+    }
+
+    /// <summary>
+    ///   <para>
+    ///     Removes the ordered pair (dependee, dependent), if it exists.
+    ///   </para>
+    /// </summary>
+    /// <param name="dependee"> The name of the node that must be evaluated first</param>
+    /// <param name="dependent"> The name of the node that cannot be evaluated until after dependee</param>
+    public void RemoveDependency(string dependee, string dependent)
+    {
+        if (this._dependees.ContainsKey(dependent) &&
+            this._dependees[dependent].Contains(dependee))
+        {
+            this._dependees[dependent].Remove(dependee);
+            this._dependents[dependee].Remove(dependent);
+            _size--;
+        }
+    }
+
+    /// <summary>
+    ///   Removes all existing ordered pairs of the form (nodeName, *).  Then, for each
+    ///   t in newDependents, adds the ordered pair (nodeName, t).
+    /// </summary>
+    /// <param name="nodeName"> The name of the node whose dependents are being replaced </param>
+    /// <param name="newDependents"> The new dependents for nodeName</param>
+    public void ReplaceDependents(string nodeName, IEnumerable<string> newDependents)
+    {
+        HashSet<string> newSet = new HashSet<string>(newDependents);
+        
+        
+        foreach (string dependent in GetDependents(nodeName))
+        {
+            RemoveDependency(nodeName, dependent);
+        }
+        
+        foreach (string dependent in newSet)
+        {
+            AddDependency(nodeName, dependent);
+        }
+    }
+
+    /// <summary>
+    ///   <para>
+    ///     Removes all existing ordered pairs of the form (*, nodeName).  Then, for each
+    ///     t in newDependees, adds the ordered pair (t, nodeName).
+    ///   </para>
+    /// </summary>
+    /// <param name="nodeName"> The name of the node who's dependees are being replaced</param>
+    /// <param name="newDependees"> The new dependees for nodeName</param>
+    public void ReplaceDependees(string nodeName, IEnumerable<string> newDependees)
+    {
+        HashSet<string> newSet = new HashSet<string>(newDependees);
+    
+        foreach (string dependee in GetDependees(nodeName))
+        {
+            RemoveDependency(dependee, nodeName);
+        }
+    
+        foreach (string dependee in newSet)
+        {
+            AddDependency(dependee, nodeName);
+        }
+    }
+}
---
+++ Spreadsheet/DependencyGraphTests/DependencyGraphTests.cs
@@ -0,0 +1,333 @@
+﻿
+namespace DependencyGraphTests;
+
+using DependencyGraph;
+
+/// <summary>
+///   This is a test class for DependencyGraphTest and is intended
+///   to contain all DependencyGraphTest Unit Tests
+/// </summary>
+[TestClass]
+public class DependencyGraphTests
+{
+    /// <summary>
+    ///   TODO:  Explain carefully what this code tests.
+    ///          Also, update in-line comments as appropriate.
+    /// </summary>
+    [TestMethod]
+    [Timeout( 2000, CooperativeCancellation = true)]  // 2 second run time limit
+    public void StressTest()
+    {
+        DependencyGraph dg = new();
+
+        // A bunch of strings to use
+        const int size = 200;
+        string[] letters = new string[size];
+        for ( int i = 0; i < size; i++ )
+        {
+            letters[i] = string.Empty + ( (char) ( 'a' + i ) );
+        }
+
+        // The correct answers
+        HashSet<string>[] dependents = new HashSet<string>[size];
+        HashSet<string>[] dependees = new HashSet<string>[size];
+        for ( int i = 0; i < size; i++ )
+        {
+            dependents[i] = [];
+            dependees[i] = [];
+        }
+
+        // Add a bunch of dependencies
+        for ( int i = 0; i < size; i++ )
+        {
+            for ( int j = i + 1; j < size; j++ )
+            {
+                dg.AddDependency( letters[i], letters[j] );
+                dependents[i].Add( letters[j] );
+                dependees[j].Add( letters[i] );
+            }
+        }
+
+        // Remove a bunch of dependencies
+        for ( int i = 0; i < size; i++ )
+        {
+            for ( int j = i + 4; j < size; j += 4 )
+            {
+                dg.RemoveDependency( letters[i], letters[j] );
+                dependents[i].Remove( letters[j] );
+                dependees[j].Remove( letters[i] );
+            }
+        }
+
+        // Add some back
+        for ( int i = 0; i < size; i++ )
+        {
+            for ( int j = i + 1; j < size; j += 2 )
+            {
+                dg.AddDependency( letters[i], letters[j] );
+                dependents[i].Add( letters[j] );
+                dependees[j].Add( letters[i] );
+            }
+        }
+
+        // Remove some more
+        for ( int i = 0; i < size; i += 2 )
+        {
+            for ( int j = i + 3; j < size; j += 3 )
+            {
+                dg.RemoveDependency( letters[i], letters[j] );
+                dependents[i].Remove( letters[j] );
+                dependees[j].Remove( letters[i] );
+            }
+        }
+
+        // Make sure everything is right
+        for ( int i = 0; i < size; i++ )
+        {
+            Assert.IsTrue( dependents[i].SetEquals( new HashSet<string>( dg.GetDependents( letters[i] ) ) ) );
+            Assert.IsTrue( dependees[i].SetEquals( new HashSet<string>( dg.GetDependees( letters[i] ) ) ) );
+        }
+    }
+
+    /// <summary>
+    /// Test that with no dependencies, the depency size is 0
+    /// expected outcome: valid
+    /// </summary>
+    [TestMethod]
+    public void DependencyGraph_GraphSizeZero_Valid()
+    {
+        DependencyGraph dg = new DependencyGraph();
+        Assert.AreEqual(0, dg.Size);
+    }
+    
+    /// <summary>
+    /// tests when adding one dependency, that size is updated
+    /// expected outcome: valid
+    /// </summary>
+    [TestMethod]
+    public void DependencyGraph_GraphSizeOne_Valid()
+    {
+        DependencyGraph dg = new DependencyGraph();
+        dg.AddDependency("a1", "a2");
+        Assert.AreEqual(1, dg.Size);
+    }
+    
+    /// <summary>
+    /// Tests when adding a duplicate dependency, size is NOT
+    /// updated
+    /// expected outcome: valid
+    /// </summary>
+    [TestMethod]
+    public void DependencyGraph_GraphSizeDuplicateOne_Valid()
+    {
+        DependencyGraph dg = new DependencyGraph();
+        dg.AddDependency("a1", "a2");
+        dg.AddDependency("a1", "a2");
+        Assert.AreEqual(1, dg.Size);
+    }
+    
+    /// <summary>
+    /// Tests that getDependees returns the correct variables
+    /// expected outcome: valid
+    /// </summary>
+    [TestMethod]
+    public void DependencyGraph_TestGetDependees_Valid()
+    {
+        DependencyGraph dg = new DependencyGraph();
+        dg.AddDependency("a1", "a2");
+        dg.AddDependency("b1", "a2");
+        IEnumerable<string> result = dg.GetDependees("a2");
+        var enumerable = result.ToList();
+        Assert.AreEqual("a1", enumerable.First());
+        Assert.AreEqual("b1", enumerable.Last());
+    }
+    
+    /// <summary>
+    /// Tests that getDependents returns the correct variables
+    /// expected outcome: valid
+    /// </summary>
+    [TestMethod]
+    public void DependencyGraph_TestGetDependents_Valid()
+    {
+        DependencyGraph dg = new DependencyGraph();
+        dg.AddDependency("a1", "a2");
+        dg.AddDependency("a1", "b2");
+        IEnumerable<string> result = dg.GetDependents("a1");
+        var enumerable = result.ToList();
+        Assert.AreEqual("a2", enumerable.First());
+        Assert.AreEqual("b2", enumerable.Last());
+    }
+    
+    /// <summary>
+    /// Tests that removing a depency decreases size
+    /// expected outcome: valid
+    /// </summary>
+    [TestMethod]
+    public void DependencyGraph_TestRemoveDependentsSize_Valid()
+    {
+        DependencyGraph dg = new DependencyGraph();
+        dg.AddDependency("a1", "a2");
+        dg.AddDependency("a1", "b2");
+        dg.RemoveDependency("a1", "a2");
+        Assert.AreEqual(1, dg.Size);
+    }
+    
+    /// <summary>
+    /// Tests that removing a non-existent dependent does nothing
+    /// expected outcome: valid
+    /// </summary>
+    [TestMethod]
+    public void DependencyGraph_TestRemoveNonDependents_Valid()
+    {
+        DependencyGraph dg = new DependencyGraph();
+        dg.AddDependency("a1", "a2");
+        dg.RemoveDependency("a1", "b2");
+        Assert.AreEqual(1, dg.Size);
+        IEnumerable<string> result = dg.GetDependents("a1");
+        var enumerable = result.ToList();
+        Assert.AreEqual(1, enumerable.Count());
+        Assert.AreEqual("a2", enumerable.First());
+        
+    }
+    
+    /// <summary>
+    /// Tests replaceDependents works and that both HashSets are updated
+    /// expected outcome: valid
+    /// </summary>
+    [TestMethod]
+    public void DependencyGraph_TestReplaceDependents_Valid()
+    {
+        DependencyGraph dg = new DependencyGraph();
+        dg.AddDependency("a1", "a2");
+        dg.AddDependency("a1", "a3");
+
+        List<string> newDependents = new List<string>();
+        newDependents.Add("b1");
+        newDependents.Add("b2");
+        dg.ReplaceDependents("a1", newDependents);
+        IEnumerable<string> result = dg.GetDependents("a1");
+        var enumerable = result.ToList();
+        Assert.AreEqual("b2", enumerable.First());
+        Assert.AreEqual("b1", enumerable.Last());
+        
+        Assert.DoesNotContain("a2", enumerable);
+        Assert.DoesNotContain("a3", enumerable);
+        
+        Assert.IsTrue(dg.GetDependees("b1").Contains("a1"));
+        Assert.IsTrue(dg.GetDependees("b2").Contains("a1"));
+    }
+    
+    /// <summary>
+    /// Tests replaceDependees works and that both HashSets are updated
+    /// expected outcome: valid
+    /// </summary>
+    [TestMethod]
+    public void DependencyGraph_TestReplaceDependees_Valid()
+    {
+        DependencyGraph dg = new DependencyGraph();
+        dg.AddDependency("a2", "a1");
+        dg.AddDependency("a3", "a1");
+
+        List<string> newDependees = new List<string>();
+        newDependees.Add("b1");
+        newDependees.Add("b2");
+        dg.ReplaceDependees("a1", newDependees);
+        IEnumerable<string> result = dg.GetDependees("a1");
+        var enumerable = result.ToList();
+        Assert.AreEqual("b2", enumerable.First());
+        Assert.AreEqual("b1", enumerable.Last());
+        
+        Assert.DoesNotContain("a2", enumerable);
+        Assert.DoesNotContain("a3", enumerable);
+        
+        Assert.IsTrue(dg.GetDependents("b1").Contains("a1"));
+        Assert.IsTrue(dg.GetDependents("b2").Contains("a1"));
+
+    }
+    
+    /// <summary>
+    /// Tests that the dependee and dependent can be the same value
+    /// and everything works as intended
+    /// expected outcome: valid
+    /// </summary>
+    [TestMethod]
+    public void DependencyGraph_TestDependeeDependentAreSame_Valid()
+    {
+        DependencyGraph dg = new DependencyGraph();
+        dg.AddDependency("a1", "a1");
+        
+        Assert.AreEqual(1, dg.Size);
+        Assert.IsTrue(dg.HasDependees("a1"));
+        Assert.IsTrue(dg.HasDependents("a1"));
+
+        IEnumerable<string> resultDependents = dg.GetDependents("a1");
+        var enumerableDependents = resultDependents.ToList();
+        Assert.AreEqual("a1", enumerableDependents.First());
+        
+        IEnumerable<string> resultDependees = dg.GetDependees("a1");
+        var enumerableDependees = resultDependees.ToList();
+        Assert.AreEqual("a1", enumerableDependees.First());
+        
+
+    }
+    
+    /// <summary>
+    /// this test checks that an empty dependent that was never added
+    /// returns false 
+    /// expected outcome: valid
+    /// </summary>
+    [TestMethod]
+    public void DependencyGraph_TestDependentIsEmpty_Valid()
+    {
+        DependencyGraph dg = new DependencyGraph();
+        
+        Assert.IsFalse(dg.HasDependents("a1"));
+
+    }
+    
+    /// <summary>
+    /// this test checks that an empty dependee that was never added
+    /// returns false 
+    /// expected outcome: valid
+    /// </summary>
+    [TestMethod]
+    public void DependencyGraph_TestDependeeIsEmpty_Valid()
+    {
+        DependencyGraph dg = new DependencyGraph();
+        
+        Assert.IsFalse(dg.HasDependees("a1"));
+
+    }
+    
+    /// <summary>
+    /// this test checks that a dependent that was added then removed
+    /// properly returns with no dependents
+    /// expected outcome: valid
+    /// </summary>
+    [TestMethod]
+    public void DependencyGraph_TestDependentWhenEmpty_Valid()
+    {
+        DependencyGraph dg = new DependencyGraph();
+        dg.AddDependency("a1", "b1");
+        dg.RemoveDependency("a1", "b1");
+        
+        Assert.IsFalse(dg.HasDependents("a1"));
+
+    }
+    
+    /// <summary>
+    /// this test checks that a dependee that was added then removed
+    /// properly returns with no dependees
+    /// expected outcome: valid
+    /// </summary>
+    [TestMethod]
+    public void DependencyGraph_TestDependeeWhenEmpty_Valid()
+    {
+        DependencyGraph dg = new DependencyGraph();
+        dg.AddDependency("a1", "b1");
+        dg.RemoveDependency("a1", "b1");
+        
+        Assert.IsFalse(dg.HasDependees("b1"));
+
+    }
+}
